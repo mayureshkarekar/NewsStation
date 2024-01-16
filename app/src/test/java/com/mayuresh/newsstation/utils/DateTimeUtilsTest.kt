2cd5cd6ec339b898174416f97ffc0d29f0af5e46
Mayuresh Karekar <mkarekar95@gmail.com>
2024-01-17 01:24:00 +0530

    1. Minor fixes in DateTimeUtilsTest.

--- app/src/test/java/com/mayuresh/newsstation/utils/DateTimeUtilsTest.kt
+++ app/src/test/java/com/mayuresh/newsstation/utils/DateTimeUtilsTest.kt
@@ -32,17 +32,17 @@ class DateTimeUtilsTest(
         fun input(): List<Array<Any?>> {
             return listOf(
                 // Valid formats.
-                arrayOf("2022-01-15T14:30:00", "Sat, 15 Jan 2022"), // Pass
-                arrayOf("2023-07-05T08:45:20", "Wed, 05 Jul 2023"), // Pass
-                arrayOf("2024-12-01T19:00:45", "Sun, 01 Dec 2024"), // Pass
-                arrayOf("2025-09-20T10:15:30", "Sat, 20 Sep 2025"), // Pass
-                arrayOf("2026-03-10T22:05:55", "Tue, 10 Mar 2026"), // Pass
+                arrayOf("2022-01-15T14:30:00", "Sat, 15 Jan 2022"),
+                arrayOf("2023-07-05T08:45:20", "Wed, 05 Jul 2023"),
+                arrayOf("2024-12-01T19:00:45", "Sun, 01 Dec 2024"),
+                arrayOf("2025-09-20T10:15:30", "Sat, 20 Sep 2025"),
+                arrayOf("2026-03-10T22:05:55", "Tue, 10 Mar 2026"),
 
                 // Invalid formats.
-                arrayOf("13-25-2025", null), // Pass
-                arrayOf("2023-02-30T08:45", null), // Pass
-                arrayOf("2025-04-15T12:70:00", "Sat, 20 Sep 2025"), // Fail
-                arrayOf("2026-08-10T24:30:45", "Tue, 10 Mar 2026")// Fail
+                arrayOf("13-25-2025", null),
+                arrayOf("2023-02-30T08:45", null),
+                arrayOf("20250415T127000", null),
+                arrayOf("", null)// Fail
             )
         }
     }
